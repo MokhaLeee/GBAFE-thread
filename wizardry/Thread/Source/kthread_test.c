bd02419a5dc030030a045c8099c66aac3b5f6e3c
MokhaLeee <nkulyc@163.com>
2025-07-05 17:22:46 +0000

    Add API for yield (but maybe need polish)

--- wizardry/Thread/Source/kthread_test.c
+++ wizardry/Thread/Source/kthread_test.c
@@ -4,22 +4,5 @@
 void kthread_test(void)
 {
 	while (1)
-		;
-}
-
-// lynjump
-void InitUnits(void)
-{
-	int i;
-
-	CreateSubThread(kthread_test);
-
-	for (i = 0; i < 0x100; i++) {
-		struct Unit *unit = GetUnit(i);
-
-		if (unit) {
-			ClearUnit(unit);
-			unit->index = i;
-		}
-	}
+		YieldSubThread();
 }
